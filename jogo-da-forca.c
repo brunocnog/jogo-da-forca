bd5f076028d856545145c203249471d34005638c
brunocnog <brunocesar.br@gmail.com>
2023-01-26 18:06:19 -0300

    Criando funções
    Criada mais funções para enxugar o código.

--- jogo-da-forca.c
+++ jogo-da-forca.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
+//protótipo das funções
+void abertura ();
+
+void chuta(char chutes[], int* tentativas);
+
+int jachutou(char letra, char *chutes, int tentativas);
+
+int main(){
+
+    char palavrasecreta[20];
+    //tem a mesma função do printf, contudo ao invez de imprimir na tela, armazena os caracteres no buffer
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0, enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    abertura();
+        
+    do {
+    
+        //loop para imprimir a palavra secreta
+        for(int i = 0; i <strlen(palavrasecreta); i++){
+
+           if(jachutou(palavrasecreta[i], chutes, tentativas)){
+                printf("%c ", palavrasecreta[i]);
+           }else {
+                printf("_ ");
+            }
+        }                
+        printf("\n\n");
+            
+        chuta(chutes, &tentativas);
+
+    } while (!acertou && !enforcou);
+        
+}
+
+/* FUNÇÕES */
+
 void abertura (){
     printf("************************\n");
     printf("*     Jogo da Forca    *\n");
@@ -19,46 +60,16 @@ void chuta(char chutes[], int* tentativas){
     (*tentativas)++;
 }
 
-int main(){
+//verifica se o jogador já chutou determinada letra
+int jachutou(char letra, char *chutes, int tentativas){
 
-    char palavrasecreta[20];
-    //tem a mesma função do printf, contudo ao invez de imprimir na tela, armazena os caracteres no buffer
-    sprintf(palavrasecreta, "MELANCIA");
+    int achou = 0;
 
-    int acertou = 0, enforcou = 0;
-
-    char chutes[26];
-    int tentativas = 0;
-
-    abertura();
-        
-    do {
-    
-        printf("Voce ja deu %d chutes.\n", tentativas);
-        //loop para imprimir a palavra secreta
-        for(int i = 0; i <strlen(palavrasecreta); i++){
-
-            //teste booleano para o if abaixo   
-            int achou = 0; //false
-        
-            //armazena a palavra secreta dentro do array de char chutes
-            for(int j = 0; j < tentativas; j++){
-                if(chutes[j] == palavrasecreta[i]){
-                    achou = 1; //true
-                    break;
-                }
-            }
-            
-            if(achou){
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }                
-        printf("\n\n");
-            
-        chuta(chutes, &tentativas);
-
-    } while (!acertou && !enforcou);
-        
+    for(int j = 0; j < tentativas; j++){
+        if(chutes[j] == letra){
+            achou = 1;
+            break;
+        }
+    }
+    return achou;
 }
